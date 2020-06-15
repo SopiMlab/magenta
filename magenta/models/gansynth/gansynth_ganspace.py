469063a353d707506f75447c493a1f721dd2fcdf
miranda kastemaa <miranda@foldplop.com>
2020-06-15 12:55:09 +0300

    support incremental PCA

--- magenta/models/gansynth/gansynth_ganspace.py
+++ magenta/models/gansynth/gansynth_ganspace.py
@@ -1,4 +1,4 @@
-from functools import reduce
+import math
 import os
 import pickle
 import sys
@@ -32,26 +32,37 @@ absl.flags.DEFINE_string(
 absl.flags.DEFINE_integer(
   "batch_size",
   8,
-  "Batch size for generation.")
+  "Batch size for generation."
+)
 absl.flags.DEFINE_integer(
   "random_z_count",
   None,
   "Number of random latent vectors to sample."
 )
-absl.flags.DEFINE_string(
-  "activations_in_file",
-  None,
-  ".npy file to load activations from"
-)
-absl.flags.DEFINE_string(
-  "activations_out_file",
-  None,
-  ".npy file to save activations to"
-)
+# absl.flags.DEFINE_string(
+#   "activations_in_file",
+#   None,
+#   ".npy file to load activations from"
+# )
+# absl.flags.DEFINE_string(
+#   "activations_out_file",
+#   None,
+#   ".npy file to save activations to"
+# )
 absl.flags.DEFINE_string(
   "pca_out_file",
   None,
   ".pickle file to save PCA result to"
+)
+absl.flags.DEFINE_string(
+  "estimator",
+  "fbpca",
+  "PCA estimator to use"
+)
+absl.flags.DEFINE_integer(
+  "pca_batch_size",
+  None,
+  "Batch size for PCA (if using an estimator with batch support)"
 )
 absl.flags.DEFINE_integer(
   "pitch",
@@ -67,17 +78,46 @@ absl.flags.DEFINE_integer(
 FLAGS = absl.flags.FLAGS
 tf.logging.set_verbosity(tf.logging.INFO)
 
-product = lambda xs: reduce(lambda a, b: a*b, xs, 1)
-  
+def generate_activations(model, layer, pitch, n):
+  zs = model.generate_z(n)
+  pitches = np.array([pitch] * n)
+  #logging.info("batch_size = {}".format(FLAGS.batch_size))
+  #logging.info("zs.shape = {}".format(zs.shape))
+  #logging.info("pitches.shape = {}".format(pitches.shape))
+
+  # returns a rank 4 array, e.g. layer conv1_2 has shape (random_z_count, 4, 32, 256)
+  activations = model.generate_layer_outputs_from_z(
+    zs,
+    pitches,
+    layer_names=[layer]
+  )[layer]
+  return activations
+
+def generate_activations_batches(model, layer, pitch, batch_size, n):
+  n_batches = math.ceil(n / batch_size)
+  for i in range(n_batches):
+    logging.info("generating activations batch {}/{}".format(i+1, n_batches))
+    yield generate_activations(model, layer, pitch, batch_size)
+
+def reshape_for_pca(activations, activation_shape):
+  logging.info("activations.shape = {}".format(activations.shape))
+
+  # reshape to rank 2 for PCA, preserving the first dimension and flattening the rest
+  activations = activations.reshape((activations.shape[0], np.prod(activation_shape)))
+      
+  logging.info("reshaped activations.shape = {}".format(activations.shape))
+
+  return activations
+    
 def main(unused_argv):
   absl.flags.FLAGS.alsologtostderr = True
-
+  
   if FLAGS.seed != None:
     np.random.seed(FLAGS.seed)
     tf.random.set_random_seed(FLAGS.seed)
 
-  if sum((int(f) for f in [FLAGS.list_layers, FLAGS.random_z_count != None, FLAGS.activations_in_file != None])) != 1:
-    logging.info("exactly one of --list_layers, --random_z_count or --activations_in_file must be specified")
+  if sum((int(f) for f in [FLAGS.list_layers, FLAGS.random_z_count != None])) != 1:
+    logging.info("exactly one of --list_layers or --random_z_count must be specified")
     sys.exit(1)
 
   model = None
@@ -94,45 +134,49 @@ def main(unused_argv):
       logging.info("  shape: {}".format(layer.shape))
       logging.info("  min. random_z_count: {}".format(product(layer.shape[1:])))
     return
-  elif FLAGS.random_z_count != None:
-    zs = model.generate_z(FLAGS.random_z_count)
-    pitches = np.array([FLAGS.pitch] * FLAGS.random_z_count)
-    logging.info("batch_size = {}".format(FLAGS.batch_size))
-    logging.info("zs.shape = {}".format(zs.shape))
-    logging.info("pitches.shape = {}".format(pitches.shape))
 
-    # returns a rank 4 array, e.g. layer conv1_2 has shape (random_z_count, 4, 32, 256)
-    activations = model.generate_layer_outputs_from_z(
-      zs,
-      pitches,
-      layer_names=[FLAGS.layer]
-    )[FLAGS.layer]
-  elif FLAGS.activations_in_file != None:
-    activations = np.load(FLAGS.activations_in_file)
+  assert FLAGS.random_z_count != None
 
-  logging.info("activations.shape = {}".format(activations.shape))
+  activation_shape = model.fake_data_endpoints[FLAGS.layer].shape.as_list()[1:]
+  n_components = np.prod(activation_shape)
+  logging.info("activation_shape = {}".format(activation_shape))
+  logging.info("n_components = {}".format(n_components))
+
+  # only applies to spca estimator but we need to provide it anyway
+  sparsity = 1.0
   
-  activation_shape = activations.shape[1:]
+  estimator = estimators.get_estimator(FLAGS.estimator, n_components, sparsity)
   
-  # reshape to rank 2 for PCA, preserving the first dimension and flattening the rest
-  activations = activations.reshape((
-    activations.shape[0],
-    product(activations.shape[1:])
-  ))
+  if estimator.batch_support:
+    pca_batch_size = FLAGS.pca_batch_size or n_components
+    
+    assert pca_batch_size >= n_components
+    assert FLAGS.random_z_count % pca_batch_size == 0, "random_z_count={} is not evenly divisible by pca_batch_size={}".format(FLAGS.random_z_count, pca_batch_size)
+
+    n = FLAGS.random_z_count
+    
+    for activations in generate_activations_batches(model, FLAGS.layer, FLAGS.pitch, pca_batch_size, FLAGS.random_z_count):
+      activations = reshape_for_pca(activations, activation_shape)
+      logging.info("fit_partial()")
+      if not estimator.fit_partial(activations):
+        # fit_partial() should print an error if it fails, so just exit
+        sys.exit(1)
+
+    global_mean = estimator.transformer.mean_
+  else:
+    activations = generate_activations(model, FLAGS.layer, FLAGS.pitch, FLAGS.random_z_count)
+    activations = reshape_for_pca(activations, activation_shape)
+        
+    n = activations.shape[0]
+    
+    assert n >= n_components
+
+    # subtract mean
+    global_mean = activations.mean(axis=0, keepdims=True, dtype=np.float32)
+    activations -= global_mean
   
-  logging.info("reshaped activations.shape = {}".format(activations.shape))
-
-  n, n_components = activations.shape
-
-  assert n >= n_components
-
-  # subtract mean
-  global_mean = activations.mean(axis=0, keepdims=True, dtype=np.float32)
-  activations -= global_mean
-  
-  logging.info("running estimator")
-  estimator = estimators.FacebookPCAEstimator(n_components)
-  estimator.fit(activations)
+    logging.info("running estimator")
+    estimator.fit(activations)
 
   logging.info("getting components")
   comp, stdev, var_ratio = estimator.get_components()
@@ -146,9 +190,11 @@ def main(unused_argv):
   
   pca_dict = {
     "layer": FLAGS.layer,
+    "estimator": FLAGS.estimator,
     "comp": comp,
     "stdev": stdev,
-    "var_ratio": var_ratio
+    "var_ratio": var_ratio,
+    "global_mean": global_mean
   }
 
   logging.info("pca_dict = {}".format(pca_dict))
